ed1e500993577f246ebe690efe2b903abe5362a8
svatoslavarosenko <quex.modest@gmail.com>
2025-02-06 00:09:23 +0200

    added: profile-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProfileCardComponent} from './profile-card/profile-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tik-talk';
+  title: string = 'tik-talk';
+  protected readonly array: Array <number> = array;
 }
+
+const array: Array <number> = [1, 2, 3];
---
+++ src/app/profile-card/profile-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card',
+  imports: [],
+  standalone: true,
+  templateUrl: './profile-card.component.html',
+  styleUrl: './profile-card.component.scss'
+})
+export class ProfileCardComponent {
+
+}
